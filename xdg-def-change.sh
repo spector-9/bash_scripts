9413ad647b8e4a8a0d82a5191b3a34b3299bd0d9
spector-9 <arshkumar9@live.com>
2022-07-01 19:23:55 +0530

    Fixed possible globbing issue

--- xdg-def-change.sh
+++ xdg-def-change.sh
@@ -4,17 +4,17 @@ filetype () {
 }
 
 query() {
-    xdg-mime query default $(filetype "$@")
+    xdg-mime query default "$(filetype "$@")"
 }
 
 
 set_default() {
     name_default_new="$1"
     shift
-    xdg-mime default "$name_default_new" $(filetype "$@")
-    echo "Filetype for $(filetype $@) set to $name_default_new"
+    xdg-mime default "$name_default_new" "$(filetype "$@")"
+    echo "Filetype for $(filetype "$@") set to $name_default_new"
 }
 
-selection=$1
+selection="$1"
 shift
-([ $selection = "query" ] && query "$@") || ([ $selection = "set" ] && set_default "$@")
+([ "$selection" = "query" ] && query "$@") || ([ "$selection" = "set" ] && set_default "$@")
